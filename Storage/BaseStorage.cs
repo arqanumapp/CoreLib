37ea2ee50587ad58439634f24ddf3fec12fa6a09
danil54543 <danil54543@gmail.com>
2025-05-08 20:58:45 +0300

    add BaseStorage

---
+++ Storage/BaseStorage.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace CoreLib.Storage
+{
+    class BaseStorage<T> where T : new()
+    {
+        protected readonly SQLiteAsyncConnection _database;
+        protected BaseStorage()
+        {
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string databasePath = Path.Combine(folderPath, "LocalStorage.db");
+            _database = new SQLiteAsyncConnection(databasePath);
+            _database.CreateTableAsync<T>().Wait();
+        }
+    }
+}
