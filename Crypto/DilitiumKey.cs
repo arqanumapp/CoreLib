5213f2ef86b6ec1bd7d3eec707474cc237df5e7f
danil54543 <danil54543@gmail.com>
2025-05-08 20:53:37 +0300

    implemented DilitiumKey class

---
+++ Crypto/DilitiumKey.cs
@@ -0,0 +1,66 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Security;
+
+namespace CoreLib.Crypto
+{
+    internal class DilitiumKey
+    {
+        private static readonly SecureRandom Random = new SecureRandom();
+
+        public async Task<(AsymmetricKeyParameter PublicKey, AsymmetricKeyParameter PrivateKey)> GenerateKeyPairAsync()
+        {
+            return await Task.Run(() =>
+            {
+                var keyGen = new MLDsaKeyPairGenerator();
+                keyGen.Init(new MLDsaKeyGenerationParameters(Random, MLDsaParameters.ml_dsa_87));
+                AsymmetricCipherKeyPair keyPair = keyGen.GenerateKeyPair();
+                return (keyPair.Public, keyPair.Private);
+            });
+        }
+
+        public async Task<byte[]> SignAsync(byte[] data, AsymmetricKeyParameter privateKey)
+        {
+            return await Task.Run(() =>
+            {
+                var signer = new MLDsaSigner(MLDsaParameters.ml_dsa_87, true);
+                signer.Init(true, privateKey);
+                signer.BlockUpdate(data, 0, data.Length);
+                return signer.GenerateSignature();
+            });
+        }
+
+        public async Task<bool> VerifyAsync(byte[] publicKeyBytes, byte[] message, byte[] signature)
+        {
+            return await Task.Run(async () =>
+            {
+                var publicKey = await RecoverPublicKeyAsync(publicKeyBytes);
+                var verifier = new MLDsaSigner(MLDsaParameters.ml_dsa_87, false);
+                verifier.Init(false, publicKey);
+                verifier.BlockUpdate(message, 0, message.Length);
+                return verifier.VerifySignature(signature);
+            });
+        }
+
+
+
+        public async Task<MLDsaPublicKeyParameters> RecoverPublicKeyAsync(byte[] publicKeyBytes)
+        {
+            return await Task.Run(() =>
+            {
+                return MLDsaPublicKeyParameters.FromEncoding(MLDsaParameters.ml_dsa_87, publicKeyBytes);
+            });
+        }
+
+        public async Task<MLDsaPrivateKeyParameters> RecoverPrivateKeyAsync(byte[] privateKeyBytes)
+        {
+            return await Task.Run(() =>
+            {
+                return MLDsaPrivateKeyParameters.FromEncoding(MLDsaParameters.ml_dsa_87, privateKeyBytes);
+            });
+        }
+
+    }
+}
