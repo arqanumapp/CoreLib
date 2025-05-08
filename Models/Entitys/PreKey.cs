06061dbbbbf643cd0215c3a71096b2026149bb20
danil54543 <danil54543@gmail.com>
2025-05-08 20:57:34 +0300

    add package sqlite-net-pcl, add class PreKey

---
+++ Models/Entitys/PreKey.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+
+namespace CoreLib.Models.Entitys
+{
+    internal class PreKey
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+
+        [NotNull]
+        public byte[] PK { get; set; }
+
+        [NotNull]
+        public byte[] PrK { get; set; }
+
+        [NotNull]
+        public byte[] Signature { get; set; }
+    }
+}
