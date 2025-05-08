ac4a20fa51ab426404f9092a9a7944c84b5bf2e6
danil54543 <danil54543@gmail.com>
2025-05-08 21:08:37 +0300

    implemented PreKeyStorage

---
+++ Storage/PreKeyStorage.cs
@@ -0,0 +1,46 @@
+﻿using CoreLib.Models.Entitys;
+
+namespace CoreLib.Storage
+{
+    internal class PreKeyStorage : BaseStorage<PreKey>
+    {
+        public async Task<bool> SavePreKeyAsync(PreKey preKey)
+        {
+            try
+            {
+                var result = await _database.InsertAsync(preKey);
+                return result > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<PreKey?> GetPreKeyAsync(string id)
+        {
+            try
+            {
+                var preKey = await _database.Table<PreKey>().FirstOrDefaultAsync(x => x.Id == id);
+                return preKey;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> AddRangeAsync(List<PreKey> preKeys)
+        {
+            try
+            {
+                var result = await _database.InsertAllAsync(preKeys);
+                return result > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
